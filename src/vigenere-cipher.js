1c672dab525df200ee216f82d560e3b939d2984a
Ihor Novikov <novikov8118@gmail.com>
2020-09-28 19:36:43 +0300

    feat: add solution to hanoi-tower

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -8,31 +8,30 @@ class VigenereCipheringMachine {
         if (!str || !key) {
             throw new Error('Please set arguments')
         }
-        if (this.type) {
-            let abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-            let result = []
-            let numberStr = str.toLowerCase().replace(/\W|\d/g, '').split('').map(i => i = abc.indexOf(i))
-            let cipher = ''.padStart(numberStr.length, key).toLowerCase();
-            let numberCipher = cipher.split('').map(i => i = abc.indexOf(i))
 
-            let indexArr = [...new Set(str.split('').map((i, inx) => i.match(/\W|\d/) ? inx : ''))].splice(1)
+        let abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        let result = []
+        let numberStr = str.toLowerCase().replace(/\W|\d|/g, '').split('').map(i => i = abc.indexOf(i))
+        let cipher = ''.padStart(numberStr.length, key).toLowerCase();
+        let numberCipher = cipher.split('').map(i => i = abc.indexOf(i))
 
-            let symbolArr = str.match(/\W|\d/g)
+        let indexArr = [...new Set(str.split('').map((i, inx) => i.match(/\W|\d/) ? inx : ''))].splice(1)
 
-            for (let i = 0; i < numberCipher.length; i++) {
+        let symbolArr = str.match(/\W|\d/g)
 
-                result[i] = abc[(numberStr[i] + numberCipher[i]) % abc.length].toUpperCase()
-            }
+        for (let i = 0; i < numberCipher.length; i++) {
 
-
-            for (let j = 0; j < indexArr.length; j++) {
-                result.splice(indexArr[j], 0, symbolArr[j])
-            }
-            if (this.type) { return result.join('') }
-            return result.reverse().join('')
+            result[i] = abc[(numberStr[i] + numberCipher[i]) % abc.length].toUpperCase()
         }
-    }
 
+
+        for (let j = 0; j < indexArr.length; j++) {
+            result.splice(indexArr[j], 0, symbolArr[j])
+        }
+
+        return this.type ? result.join('') : result.reverse().join('')
+
+    }
 
     decrypt(str, key) {
         if (!str || !key) {
@@ -41,14 +40,15 @@ class VigenereCipheringMachine {
 
         let abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
         let result = []
-        let numberStr = str.toLowerCase().replace(/\W/g, '').split('').map(i => i = abc.indexOf(i))
+        let numberStr = str.toLowerCase().replace(/\W|\d/g, '').split('').map(i => i = abc.indexOf(i))
         let cipher = ''.padStart(numberStr.length, key);
         let numberCipher = cipher.split('').map(i => i = abc.indexOf(i))
 
 
-        let indexArr = [...new Set(str.split('').map((i, inx) => i.match(/\W/) ? inx : ''))].splice(1)
 
-        let symbolArr = str.match(/\W/g)
+        let indexArr = [...new Set(str.split('').map((i, inx) => i.match(/\W|\d/) ? inx : ''))].splice(1)
+
+        let symbolArr = str.match(/\W|\d/g)
 
         for (let i = 0; i < numberCipher.length; i++) {
 
@@ -59,16 +59,9 @@ class VigenereCipheringMachine {
         for (let j = 0; j < indexArr.length; j++) {
             result.splice(indexArr[j], 0, symbolArr[j])
         }
-        if (this.type) { return result.join('') }
-        return result.reverse().join('')
+        return this.type ? result.join('') : result.reverse().join('')
 
     }
 }
-
-
-
-
-
-
 
 module.exports = VigenereCipheringMachine;
