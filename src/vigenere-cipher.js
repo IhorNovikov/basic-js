fbd1b05d07bdd9bb4b1cfc64f5de6ca7825dfe7b
Ihor Novikov <novikov8118@gmail.com>
2020-10-01 21:52:48 +0300

    feat: add solution to vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,54 +3,32 @@ const CustomError = require("../extensions/custom-error");
 class VigenereCipheringMachine {
     constructor(type = true) {
         this.type = type
-        this.abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+        this.abc = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
     }
     encrypt(str, key) {
         return this.method(str, key, 'encrypt')
     }
 
     decrypt(str, key) {
-
-        return this.method(str, key, 'decrypt')
+        return this.method(str, key)
     }
 
-    method(str, key, variant) {
+    method(str, key, process) {
         if (!str || !key) {
             throw new Error('Please set arguments')
         }
-
+        let keyToIndex = ''.padStart(str.length, key.toUpperCase()).split('').map(i => this.abc.indexOf(i))
+        let index = 0
         let result = []
-        let numberStr = str.toUpperCase().split('').filter(i => this.abc.includes(i)).map(i => i = this.abc.indexOf(i))
-        let cipher
-        if (key.length < str.length) {
-            cipher = ''.padStart(numberStr.length, key).toUpperCase();
-        } else cipher = key.toUpperCase();
-        let numberCipher = cipher.split('').map(i => i = this.abc.indexOf(i))
-
-
-
-        let indexArr = [...new Set(str.split('').map((i, inx) => i.match(/\W|\d/) ? inx : ''))].splice(1)
-
-        let symbolArr = str.match(/\W|\d/g)
-
-        for (let i = 0; i < numberCipher.length; i++) {
-            variant === 'encrypt' ?
-                result[i] = this.abc[(numberStr[i] + numberCipher[i]) % this.abc.length] :
-                result[i] = this.abc[(numberStr[i] - numberCipher[i] + this.abc.length) % this.abc.length]
-        }
-        for (let j = 0; j < indexArr.length; j++) {
-            result.splice(indexArr[j], 0, symbolArr[j])
-        }
+        str.toUpperCase().split('').map(i => {
+            if (this.abc.includes(i)) {
+                process ? i = ((this.abc.indexOf(i) + keyToIndex[index++]) % this.abc.length) :
+                    i = ((this.abc.indexOf(i) - keyToIndex[index++] + this.abc.length) % this.abc.length)
+                result.push(this.abc[i])
+            } else result.push(i)
+        })
         return this.type ? result.join('') : result.reverse().join('')
-
     }
-
-
-
-
-
-
-
 }
 
 module.exports = VigenereCipheringMachine;
